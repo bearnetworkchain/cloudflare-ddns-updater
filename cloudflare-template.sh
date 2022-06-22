ee68f28021bdde24b72a9e7a7a57d3b95fa3d7d2
BearNetwork <105615886+bearnetworkchain@users.noreply.github.com>
2022-06-22 20:38:03 +0800

    Update cloudflare-template.sh

--- cloudflare-template.sh
+++ cloudflare-template.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
-## change to "bin/sh" when necessary
+## 必要時更改為“bin/sh”
 
-auth_email=""                                       # The email used to login 'https://dash.cloudflare.com'
-auth_method="token"                                 # Set to "global" for Global API Key or "token" for Scoped API Token
-auth_key=""                                         # Your API Token or Global API Key
-zone_identifier=""                                  # Can be found in the "Overview" tab of your domain
-record_name=""                                      # Which record you want to be synced
-ttl="3600"                                          # Set the DNS TTL (seconds)
-proxy="false"                                       # Set the proxy to true or false
-sitename=""                                         # Title of site "Example Site"
-slackchannel=""                                     # Slack Channel #example
-slackuri=""                                         # URI for Slack WebHook "https://hooks.slack.com/services/xxxxx"
-discorduri=""                                       # URI for Discord WebHook "https://discordapp.com/api/webhooks/xxxxx"
+## 終端輸入以下指令:
+## cp cloudflare-template.sh /etc/cloudflare-bearnetwork.sh
+## chmod +x /etc/cloudflare-bearnetwork.sh
+## nano /etc/cloudflare-bearnetwork.sh
 
+
+auth_email=""                                       # 用於登錄的電子郵件'https://dash.cloudflare.com'
+auth_method="Global"                                # 為 Global API Key 設置為 "global" 或為 Scoped API Token 設置為 "token"
+auth_key=""                                         # 您的 API 令牌或全局 API 密鑰
+zone_identifier=""                                  # API 區域識別碼
+record_name=""                                      # IP更新同步的DNS
+ttl="3600"                                          # 設置DNS TTL（秒）
+proxy="true"                                        # 設置端口為true或false
 
 ###########################################
-## Check if we have a public IP
+## 檢查我們是否有公共 IP
 ###########################################
 ipv4_regex='([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\.([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])'
 ip=$(curl -s -4 https://cloudflare.com/cdn-cgi/trace | grep -E '^ip'); ret=$?
-if [[ ! $ret == 0 ]]; then # In the case that cloudflare failed to return an ip.
-    # Attempt to get the ip from other websites.
+if [[ ! $ret == 0 ]]; then # 在 cloudflare 未能返回 ip 的情況下。
+    # 嘗試從其他網站獲取IP。
     ip=$(curl -s https://api.ipify.org || curl -s https://ipv4.icanhazip.com)
 else
-    # Extract just the ip from the ip line from cloudflare.
+    # 僅從 cloudflare的 IP行中提取 IP。
     ip=$(echo $ip | sed -E "s/^ip=($ipv4_regex)$/\1/")
 fi
 
-# Use regex to check for proper IPv4 format.
+# 使用正則表達式檢查正確的 IPv4 格式。
 if [[ ! $ip =~ ^$ipv4_regex$ ]]; then
     logger -s "DDNS Updater: Failed to find a valid IP."
     exit 2
 fi
 
 ###########################################
-## Check and set the proper auth header
+## 檢查並設置正確的身份驗證頭
 ###########################################
 if [[ "${auth_method}" == "global" ]]; then
   auth_header="X-Auth-Key:"
@@ -43,7 +44,7 @@ else
 fi
 
 ###########################################
-## Seek for the A record
+## 尋找A記錄
 ###########################################
 
 logger "DDNS Updater: Check Initiated"
@@ -53,7 +54,7 @@ record=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identi
                       -H "Content-Type: application/json")
 
 ###########################################
-## Check if the domain has an A record
+## 檢查域是否有 A 記錄
 ###########################################
 if [[ $record == *"\"count\":0"* ]]; then
   logger -s "DDNS Updater: Record does not exist, perhaps create one first? (${ip} for ${record_name})"
@@ -61,7 +62,7 @@ if [[ $record == *"\"count\":0"* ]]; then
 fi
 
 ###########################################
-## Get existing IP
+## 獲取現有 IP
 ###########################################
 old_ip=$(echo "$record" | sed -E 's/.*"content":"(([0-9]{1,3}\.){3}[0-9]{1,3})".*/\1/')
 # Compare if they're the same
@@ -71,12 +72,12 @@ if [[ $ip == $old_ip ]]; then
 fi
 
 ###########################################
-## Set the record identifier from result
+## 從結果中設置記錄標識符
 ###########################################
 record_identifier=$(echo "$record" | sed -E 's/.*"id":"(\w+)".*/\1/')
 
 ###########################################
-## Change the IP@Cloudflare using the API
+## 使用 API 更改 IP@Cloudflare
 ###########################################
 update=$(curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$record_identifier" \
                      -H "X-Auth-Email: $auth_email" \
@@ -85,7 +86,7 @@ update=$(curl -s -X PATCH "https://api.cloudflare.com/client/v4/zones/$zone_iden
                      --data "{\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\",\"ttl\":\"$ttl\",\"proxied\":${proxy}}")
 
 ###########################################
-## Report the status
+## 報告狀態
 ###########################################
 case "$update" in
 *"\"success\":false"*)
